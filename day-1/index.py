5fa4943e05bb1b5d573737f93fb9ceb9d418be07
Arun-ez <arunshaw433@mail.com>
2024-02-22 23:26:25 +0530

    added day-1 test

---
+++ day-1/index.py
@@ -0,0 +1,19 @@
+# first print statement
+print('Hello World');
+print('Hello', 'Arun')
+
+#variables
+name="Arun" # -->str
+age=23 # -->int
+dec = 10.12 # -->float
+hey = False # -->bool
+nothting = None # -->none
+
+#1variavble="hello" --> not a valid variable name
+
+print('My name is', name, ",", "having age of", age);
+print("type of name", type(name));
+print("type of age", type(age));
+print("type of dec", type(dec));
+print(type(nothting));
+
